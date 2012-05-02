d539afe934b17a4c39f6a93a07aa618b01f919a7
Felipe Scuciatto <scuciatto@me.com>
2012-05-02 19:40:14 -0300

    Teste

---
+++ app.js
@@ -0,0 +1,39 @@
+// this sets the background color of the master UIView (when there are no windows/tab groups on it)
+Titanium.UI.setBackgroundColor('#000');
+
+// create tab group
+var tabGroup = Titanium.UI.createTabGroup();
+
+
+//
+// create base UI tab and root window
+//
+var win1 = Titanium.UI.createWindow({  
+    title:'Tab 1',
+    backgroundColor:'#fff'
+});
+var tab1 = Titanium.UI.createTab({  
+    icon:'/images/initial.png',
+    title:'Tab 1',
+    window:win1
+});
+
+var label1 = Titanium.UI.createLabel({
+	color:'#999',
+	text:'I am Window 1',
+	font:{fontSize:20,fontFamily:'Helvetica Neue'},
+	textAlign:'center',
+	width:'auto'
+});
+
+win1.add(label1);
+
+
+//
+//  add tabs
+//
+tabGroup.addTab(tab1);    
+
+
+// open tab group
+tabGroup.open();
